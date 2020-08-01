2cfde8f81f5d074ce7335c2eff639c8274e953dc
DuckyMomo <tienvinh.duong4@gmail.com>
2020-08-01 10:33:04 +0700

    Overload function
    overload split from Tokenizer

--- SourceCodeLib/Tokenizer.cpp
+++ SourceCodeLib/Tokenizer.cpp
@@ -11,3 +11,25 @@ vector <string> Tokenizer::split(string value, string separator) {
     result.push_back(value.substr(mark, value.size() - mark));
     return result;
 }
+
+vector<string> Tokenizer::split(string value, string separator1, string separator2) {
+    vector <string> _store;
+    int mark = value.find(separator1);
+    int head = value.find(separator2);
+    _store.push_back(value.substr(++mark, head - mark));
+    while ((head = value.find(separator2, head + separator2.size())) != value.npos) {
+        try {
+            mark = value.find(separator1, mark + separator1.size());
+            if (mark < head) {
+                _store.push_back(value.substr(++mark, head - mark));
+            }
+            else {
+                throw "Invalid format";
+            }
+        }
+        catch (const char* msg) {
+            cout << msg << endl;
+        }
+    }
+    return _store;
+}

9d9f388e6af953a89a059d884ce7036e4d52dfe8
DuckyMomo <tienvinh.duong4@gmail.com>
2020-09-19 21:05:20 +0700

    small edit

--- University/Point.cpp
+++ University/Point.cpp
@@ -74,7 +74,7 @@ void Point::clearPrintedContent() {
 }
 
 void Point::print() {
-	for (int i = 0; i < _content.size(); i++) {
+	for (unsigned i = 0; i < _content.size(); i++) {
 		goTo(_x, _y + i);
 		cout << _content[i];
 	}
